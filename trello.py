3be4226248e3b4971c202c1450495814cb19b54d
Thomas Liu <liuxk99@gmail.com>
2020-04-23 16:45:15 +0800

    [Dev]<trello/dictionary>: update(2020/04/22).

--- trello.py
+++ trello.py
@@ -219,4 +219,5 @@ trello_dict = {
     u"#SDE(Android)/DevEnv 'mstar928'": "https://trello.com/c/oQFJsSHh",
     u"#DMS(乐视) 'JIRA(HERA-3709)'·调查": "https://trello.com/c/6Asjg018",
     u"#SDE(Android)/DevEnv 'mstar918'": "https://trello.com/c/PuKlxRID",
+    u"#SDE(Android)/DevEnv 'AmlT972'": "https://trello.com/c/Qe5tiBq1",
 }
