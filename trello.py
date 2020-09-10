930fe4d3ea2ef3d688dcc75b0574d71c0f1968dc
Thomas Liu <liuxk99@gmail.com>
2020-09-10 10:16:32 +0800

    [Dev]<trello/dictionary>: update(2020/09/09).

--- trello.py
+++ trello.py
@@ -414,4 +414,6 @@ trello_dict = {
     u"#社交/亲/子(刘枢鸿) '教育'": "https://trello.com/c/ewayxAhj",
     u"#生活/用 '宽带<ISP:中国联通>'": "https://trello.com/c/HQtIGKRz",
     u"#生活/用 '缝纫机油'": "https://trello.com/c/BSFt2XiW",
+    u"#生活/行/车 '安全(i车保护神)'": "https://trello.com/c/hxBuSQ9R",
+    u"#DMS(乐视) 'JIRA(GRACE-421)'": "https://trello.com/c/lmffb4nR",
 }
