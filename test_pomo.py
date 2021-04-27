a7defa58fa96afa4fa32098740a21d2b4dc1b099
Thomas Liu <liuxk99@gmail.com>
2021-04-27 14:47:41 +0800

    [Dev]<trello/tools>: parse pomo sub action.

--- test_pomo.py
+++ test_pomo.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import re
 from unittest import TestCase
 
 from pomo import Pomo
@@ -23,3 +24,41 @@ class TestPomo(TestCase):
         print(json)
         Pomo.from_json(json)
         # self.fail()
+
+    def testcaseActions01(self):
+        pomo1 = "#SW/App 'xxx' |2021/04/26"
+        print(pomo1)
+
+        pattern = "'.+'"
+        print(pattern)
+
+        self.assertIsNotNone((re.search(pattern, pomo1)))
+        return
+
+    def testcaseActions02(self):
+        pomo1 = u"#生活/日常 '天气<应用:墨迹天气>' |2021/04/26"
+        pomo2 = u"#社会/户籍(北京) '积分落户'·申报 |2021年"
+
+        pattern = u"'.+'"
+        print(pattern)
+
+        obj = re.search(pattern, pomo1)
+        self.assertIsNotNone(obj)
+        print(obj)
+
+        obj = re.search(pattern, pomo2)
+        self.assertIsNotNone(obj)
+        print(obj)
+
+        print(pomo1)
+        pat = u"(.+)'.+'(.+)"
+        obj = re.match(pat, pomo1)
+        if obj:
+            print(obj.group(2))
+
+        print(pomo2)
+        obj = re.match(pat, pomo2)
+        if obj:
+            print(obj.group(2))
+
+        return
