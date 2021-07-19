b3aba7159273dcfed4b3cc035d8a9b771fd1a531
Thomas Liu <liuxk99@gmail.com>
2021-07-19 09:44:04 +0800

    [Dev]<trello/dictionary>: update(2021/07/09).

--- conf.py
+++ conf.py
@@ -75,7 +75,7 @@ trello_dict = {
     u"#软件/应用 '极客时间·": "https://trello.com/c/GfT9WrpO",
     u"#生活/购/超市(福源超市) '购物'": "https://trello.com/c/MGhen72x",
     u"#人文/语言(英语) 'Hello英语·": "https://trello.com/c/uKkpQauy",
-    u"#SW/Service '度·": "https://trello.com/c/eWENpRpZ",
+    u"#SW/Service '度'·": "https://trello.com/c/eWENpRpZ",
     u"#ADE(Android)/lib 'Calendar·": "https://trello.com/c/Xoa4h8jN",
     u"#SW/App 'bilibili·": "https://trello.com/c/UvDAJmq7",
     u"#生命/健康/疾病 '武汉肺炎·": "https://trello.com/c/5JcIFEyF",
@@ -842,4 +842,6 @@ trello_dict = {
     u"#产品/应用 'trello4py'·": "https://trello.com/c/MT88pzXt",
     u"#SW/App 'du4py'·": "https://trello.com/c/8N6j35tG",
     u"#社交/亲/子(刘枢鸿)/教育 '数学'·": "https://trello.com/c/Ui4cNIR0",
+    u"#人文(社会) '广告'·": "https://trello.com/c/mlthX6Il",
+    u"#Programmer/DTC/CM 'gitee'·": "https://trello.com/c/zRJzQe5g",
 }
